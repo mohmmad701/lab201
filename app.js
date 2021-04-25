9a3ceaa9e54cfad6de23488f5c11549f1fcc3a82
mohmmad701 <mohammadriziq70@yahoo.com>
2021-04-25 14:15:48 +0300

    lab1

---
+++ app.js
@@ -0,0 +1,13 @@
+let yourname =prompt('enter your name');
+console.log(yourname);
+
+let yourfavarite=prompt('enter your favarite');
+console.log(yourfavarite);
+
+let yourage=prompt('enter your age');
+console.log(yourage);
+
+ let yourplace=prompt('enter your place');
+ console.log(yourplace);
+
+alert(' hello my name is  '+yourname+' my favarite is '+yourfavarite+' my  age '+yourage+' and my place in'+yourplace );
