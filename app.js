f59c82ba7868540067e5050d9a5eb6ef504c954a
moham3d <>
2021-04-27 21:56:27 +0000

    last update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let yourname =prompt('enter your name');
+let yourname =prompt('enter your name?');
 console.log(yourname);
 
-let yourfavarite=prompt('enter your favarite');
+let yourfavarite=prompt('enter your hobby?');
 console.log(yourfavarite);
 
-let yourage=prompt('enter your age');
+let yourage=prompt('enter your age?');
 console.log(yourage);
 
- let yourplace=prompt('enter your place');
+ let yourplace=prompt('enter your place?');
  console.log(yourplace);
 
 alert(' hello my name is  '+yourname+' my favarite is '+yourfavarite+' my  age '+yourage+' and my place in'+yourplace );
